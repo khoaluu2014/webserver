e0d85bf4cf6d80c3af21a3f8dcd12dc2782cad15
khoaluu2014 <khoaulu2014@gmail.com>
2025-04-16 23:35:11 -0700

    reformat

--- Webserver.cs
+++ Webserver.cs
@@ -4,53 +4,28 @@ using System.Text;
 
 namespace Tofu.Webserver
 {
-public static class Server
-{
-    /// <summary>
-    /// A lean and mean web server.
-    /// </summary>
-    private static HttpListener listener;
-
-    /// <summary>
-    /// Returns list of IP addresses assigned to localhost network devices,
-    /// such as hardwired ethernet, wireless, etc,...
-    /// </summary>
-    private static List<IPAddress> GetLocalHostIPs()
+    public static class Server
     {
-        IPHostEntry host;
-        host = Dns.GetHostEntry(Dns.GetHostName());
-        List<IPAddress> ret = host
-                              .AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
-                              .ToList();
+        /// <summary>
+        /// A lean and mean web server.
+        /// </summary>
+        private static HttpListener listener;
 
-        return ret;
-    }
-
-    private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
-    {
-        HttpListener listener = new HttpListener();
-        listener.Prefixes.Add("http://localhost/");
-
-        localhostIPs.ForEach(ip =>
+        /// <summary>
+        /// Returns list of IP addresses assigned to localhost network devices,
+        /// such as hardwired ethernet, wireless, etc,...
+        /// </summary>
+        private static List<IPAddress> GetLocalHostIPs()
         {
-            Console.WriteLine("Listening on IP" + "http://" + ip.ToString() + "/");
-            listener.Prefixes.Add("http://" + ip.ToString() + "/");
-        });
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host
+                .AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+                .ToList();
 
-        return listener;
-    }
+            return ret;
+        }
 
-    public static int maxSimultaneousConnections = 20;
-    private static Semaphore sem = new Semaphore(
-        maxSimultaneousConnections,
-        maxSimultaneousConnections
-    );
-
-    /// <summary>
-    /// Begin listening to connections on a separate worker thread
-    /// </summary>
-    private static void Start(HttpListener listener)
-    {
         private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
         {
             HttpListener listener = new HttpListener();
@@ -64,51 +39,63 @@ public static class Server
 
             return listener;
         }
-        listener.Start();
-        Task.Run(() => RunServer(listener));
-    }
 
-    /// <summary>
-    /// Start awaiting for connections, up to the "maxSimultaneousConnections value."
-    /// This code runs in a separate thread.
-    /// </summary>
-    private static void RunServer(HttpListener listener)
-    {
-        while (true)
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(
+            maxSimultaneousConnections,
+            maxSimultaneousConnections
+        );
+
+        /// <summary>
+        /// Begin listening to connections on a separate worker thread
+        /// </summary>
+        private static void Start(HttpListener listener)
         {
-            sem.WaitOne();
-            StartConnectionListener(listener);
+            listener.Start();
+            Task.Run(() => RunServer(listener));
         }
-    }
 
-    /// <summary>
-    /// Await connections.
-    /// </summary>
-    private static async void StartConnectionListener(HttpListener listener)
-    {
-        // Wait for a connection. Return to caller while we wait
-        HttpListenerContext context = await listener.GetContextAsync();
+        /// <summary>
+        /// Start awaiting for connections, up to the "maxSimultaneousConnections value."
+        /// This code runs in a separate thread.
+        /// </summary>
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
 
-        // Release the semaphore
-        sem.Release();
+        /// <summary>
+        /// Await connections.
+        /// </summary>
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            // Wait for a connection. Return to caller while we wait
+            HttpListenerContext context = await listener.GetContextAsync();
 
-        // We have a connection, do something
-        string response = "Hello Browser!";
-        byte[] encoded = Encoding.UTF8.GetBytes(response);
-        context.Response.ContentLength64 = encoded.Length;
-        context.Response.OutputStream.Write(encoded, 0, encoded.Length);
-        context.Response.OutputStream.Close();
-    }
+            // Release the semaphore
+            sem.Release();
 
+            // We have a connection, do something
+            string response = "Hello Browser!";
+            byte[] encoded = Encoding.UTF8.GetBytes(response);
+            context.Response.ContentLength64 = encoded.Length;
+            context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+            context.Response.OutputStream.Close();
+        }
 
-    /// </summary>
-    /// Starts the web server.
-    /// </summary>
+        /// </summary>
+        /// Starts the web server.
+        /// </summary>
 
-    public static void Start()
-    {
-        List<IPAddress> localHostIPs = GetLocalHostIPs();
-        HttpListener listener = InitializeListener(localHostIPs);
-        Start(listener);
+        public static void Start()
+        {
+            List<IPAddress> localHostIPs = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localHostIPs);
+            Start(listener);
+        }
     }
 }
