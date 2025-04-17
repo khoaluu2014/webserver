b9751f506cc4b862795c441f10cc6322d551e587
khoaluu2014 <khoaulu2014@gmail.com>
2025-04-16 22:39:20 -0700

    reformatt

--- Webserver.cs
+++ Webserver.cs
@@ -3,56 +3,64 @@ using System.Net.Sockets;
 
 namespace Tofu.Webserver
 {
-public static class Server
-{
-    /// <summary>
-    /// A lean and mean web server.
-    /// </summary>
-    private static HttpListener listener;
-    private static List<IPAddress> GetLocalHostIPs()
+    public static class Server
     {
-        IPHostEntry host;
-        host = Dns.GetHostEntry(Dns.GetHostName());
-        List<IPAddress> ret = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
-    }
+        /// <summary>
+        /// A lean and mean web server.
+        /// </summary>
+        private static HttpListener listener;
 
-    private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
-    {
-        HttpListener listener = new HttpListener();
-        listener.Prefixes.Add("http://localhost/");
-
-        localhostIPs.ForEach(ip =>
+        /// <summary>
+        /// Returns list of IP addresses assigned to localhost network devices, such as hardwired ethernet, wireless, etc,...
+        /// </summary>
+        private static List<IPAddress> GetLocalHostIPs()
         {
-            Console.WriteLine("Listening on IP" + "http://" + ip.ToString() + "/");
-            listener.Prefixes.Add("http://" + ip.ToString() + "/");
-        });
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ret = host
+                .AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork)
+                .ToList();
+        }
 
-        return listener;
-    }
-
-    public static int maxSimultaneousConnections = 20;
-    private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
-
-    private static void Start(HttpListener listener)
-    {
-        listener.Start();
-        Task.Run(() => RunServer(listener));
-    }
-
-    private static void RunServer(HttpListener listener)
-    {
-        while (true)
+        private static HttpListener InitializeListener(List<IPAddress> localhostIPs)
         {
-            sem.WaitOne();
-            StartConnectionListener(listener);
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost/");
+
+            localhostIPs.ForEach(ip =>
+            {
+                Console.WriteLine("Listening on IP" + "http://" + ip.ToString() + "/");
+                listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            });
+
+            return listener;
+        }
+
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(
+            maxSimultaneousConnections,
+            maxSimultaneousConnections
+        );
+
+        private static void Start(HttpListener listener)
+        {
+            listener.Start();
+            Task.Run(() => RunServer(listener));
+        }
+
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
+
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            sem.Release();
         }
     }
-
-    private static async void StartConnectionListener(HttpListener listener)
-    {
-        HttpListenerContext context = await listener.GetContextAsync();
-        sem.Release();
-
-    }
-}
 }
